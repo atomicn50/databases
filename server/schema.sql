3ffd8c5533a767643fb9a5546bbc995c3a30c2cd
atomicn50 <tinhle46@gmail.com>
2018-08-25 10:49:29 -0700

    set up schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(50) NOT NULL
+);
+--
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user INT NOT NULL,
+  room INT NOT NULL,
+  text VARCHAR(50) NOT NULL,
+  created_at DATETIME,
+  FOREIGN KEY(user) REFERENCES users(id),
+  FOREIGN KEY (room) REFERENCES rooms (id)
+
 );
 
 /* Create other tables and define schemas for them here! */
@@ -14,4 +31,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
